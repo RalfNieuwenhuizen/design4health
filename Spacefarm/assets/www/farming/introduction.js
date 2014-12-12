2ebf5d9b7ea0165243d3caa930c85a8db77b4605
ASchutter <aschutter@gmail.com>
2014-12-12 20:52:07 +0100

    [#28] Deleted old Introduction
    There was still an introduction with capital I
    
    [#28] Deleted old Introduction
    
    [#28] Merged new master with introductory screens
    
    Manually merged introductory screen into master files, uploading in new
    branch
    
    Updated gitignore
    
    Revert "Updated gitignore"
    
    Changed color
    
    Changed screens to children, intro finetuned
    
    The screens that were popped are changed to children of screenLayer,
    except exercise screens. Intro is more fine tuned now
    
    Removed unnecessary change in sceneFarm

--- Spacefarm/assets/www/farming/introduction.js
+++ Spacefarm/assets/www/farming/introduction.js
@@ -11,6 +11,7 @@ goog.require('lime.Layer');
 goog.require('farming.Tile');
 goog.require('farming.Crop');
 goog.require('farming.Scene');
+goog.require('goog.events');
 
 /**
  * Scene elements
@@ -19,18 +20,22 @@ goog.require('farming.Scene');
 farming.Introduction = function (game) {
     goog.base(this);
     this.game = game;
+    
+    // This layer is on top of all screens
     this.introLayer = new lime.Layer();
-    this.appendChild(this.introLayer);
-    this.introPhase = 0;
-    
     game.sceneMap.appendChild(this.introLayer);
-    this.center = game.getCenterPosition();
-    this.w = new lime.Sprite().setFill(SETTINGS.color.background_layer).setSize(SETTINGS.size.background_layer).setPosition(game.getCenterPosition());
+    // This layer is attached to the landscape
+    this.introScrollLayer = new lime.Layer();
+    game.sceneMap.landLayer.appendChild(this.introScrollLayer);
     
-    this.text = new lime.Label().setFontSize(16).setPosition(this.center.x, this.center.y-15).setMultiline(true);
-
-    this.button = new farming.Button('').setColor(SETTINGS.color.button).setAction(this.buttonAction, this);
-        //.setPosition(SETTINGS.position.close_button).setSize(30,30).setText('Start');;
+    this.introPhase = 1;
+    
+    this.center = game.getCenterPosition();
+    this.w = new lime.Sprite().setFill('#FFFFFF').setSize(game.getFullSize(0.85)).setPosition(this.center.x, this.center.y-10);
+    this.text = new lime.Label().setFontSize(18).setPosition(this.center.x, this.center.y-15).setMultiline(true);
+    this.button = new farming.Button('').setColor('#999999').setAction(this.buttonAction, this);
+    
+    this.appendChild(this.introLayer);
 }
 
 goog.inherits(farming.Introduction, farming.Scene);
@@ -39,13 +44,36 @@ farming.Introduction.prototype.game = null;
 
 // The intro function has been called from sceneMap
 farming.Introduction.prototype.intro = function(){
+	
 	// The intro# function is used according to the current phase
-	this['intro'+this.introPhase]();
-	console.log('introphase: ' + this.introPhase);
+	if (this['intro'+this.introPhase])
+		this['intro'+this.introPhase]()
+		
+	else
+		console.log('next intro does not exist');
+}
+
+// The button is used: introPhase goes one step further and removes the current screen
+farming.Introduction.prototype.buttonAction = function(scene) {
+	console.log('action button used');
+	
+	// If current scene is intro: close screen
+	if (scene.game.director.getCurrentScene() == scene){
+		console.log('intro is the current scene')
+		scene.game.closeIntroduction();
+	}
+	
+	scene.game.introduction.introLayer.removeAllChildren();
+	scene.game.introduction.introScrollLayer.removeAllChildren();
+	scene.game.introduction.introPhase++;
+	console.log('intro phase is: '+scene.game.introduction.introPhase);
+	
+	// Call for the next introduction screen
+	scene.intro();
 }
 
 // First introduction screen: general story
-farming.Introduction.prototype.intro0 = function(){
+farming.Introduction.prototype.intro1 = function(){
 	this.text.setFontWeight('bold').setText("2542 AD \n \n"
 					+ "Your uncle was one of the first people \n"
 					+ "to buy land in an unknown planet and decided to \n"
@@ -58,18 +86,78 @@ farming.Introduction.prototype.intro0 = function(){
 					+ "years ago but you only heard of it now. \n \n "
 					+ "After so many years, the fields on planet Yeo are \n"
 					+ "unused and empty. Will you be able to salvage the \n"
-					+ "farm? Spend your money wisely to grow the company \n"
-					+ "and unlock new possibilities by doing the exercises.");
+					+ "farm?");
+	this.w.setPosition(this.center.x, this.center.y);
+	this.text.setPosition(this.center.x, this.center.y);
+	this.button.setPosition(this.center.x, this.center.y + this.game.getFullSize(0.37).height).setSize(60,30).setText('Start');
+	this.introLayer.setOpacity(1);
+	this.introLayer.appendChild(this.w).appendChild(this.text).appendChild(this.button);
 	
-	this.button.setPosition(SETTINGS.position.center_button)
-    .setSize(SETTINGS.size.button).setText('Start');
-	this.introLayer.setOpacity(0.85);
-	this.introLayer.appendChild(this.w)
-	.appendChild(this.text).appendChild(this.button);
+	this.game.showIntroduction();
 }
 
-// The button is used: introPhase goes one step further and removes the current screen
-farming.Introduction.prototype.buttonAction = function(scene) {
-	scene.game.introduction.introLayer.removeAllChildren();
-	scene.game.introduction.introPhase++;
+// Second screen: meet the uncle
+farming.Introduction.prototype.intro2 = function(){
+	this.text.setFontWeight('bold').setPosition(this.center.x+58, this.center.y-15).setText(
+			"Hello, welcome to Spaceville. My name \n "
+			+ "is Phil, I am your uncle. \n"
+			+ "I will help you getting through \n"
+			+ "this first period. \n \n"
+			+ "We can make your farm succesful \n"
+			+ "together. If you spend your money \n"
+			+ "wisely you unlock new challenges \n"
+			+ "and possibilities. \n \n"
+			+ "Let's have a look at your farm.");
+	
+	icon = new farming.Sprite('images/Farmer.png').setSize(150, 200).setPosition(this.center.x-165, this.center.y);
+	
+	this.w.setSize(this.game.getFullSize(0.65));
+	this.button.setPosition(this.center.x, this.center.y + 120)
+		.setSize(80,30).setText('Let\'s go!');
+	this.introLayer.setOpacity(0.85);
+	this.introLayer.appendChild(this.w)
+	.appendChild(this.text).appendChild(this.button).appendChild(icon);
+	
+	this.game.showIntroduction();
+}
+
+// Third screen: go to the inventory
+farming.Introduction.prototype.intro3 = function(){
+	var middle = this.game.sceneMap.calculate('middleTile');
+	var middleCor = this.game.sceneMap.twoDToScreen(middle.x,middle.y);
+	
+	this.text.setFontWeight('bold').setPosition(middleCor.x-142,middleCor.y).setText(
+			"This is your new farm, \n click on it to see \n your inventory");
+	
+	icon = new farming.Sprite('images/Farmer.png').setSize(100, 100).setPosition(middleCor.x-276,middleCor.y);
+	this.w.setFill('images/textbox/right_arrow.png').setSize(this.game.getFullSize(0.52).width, this.game.getFullSize(0.3).height)
+		.setPosition(middleCor.x-127, middleCor.y).setOpacity(0.85);	
+	this.introScrollLayer.setOpacity(0.85);
+	this.introScrollLayer.appendChild(this.w).appendChild(this.text).appendChild(icon);
+	
+	// Listen to the show_farm event
+	goog.events.listenOnce(this.game.source,this.game.EventType.SHOW_FARM,goog.partial(this.buttonAction,this.game.introduction));
+}
+
+// Fourth screen: show the inventory
+farming.Introduction.prototype.intro4 = function(){
+	var middle = this.game.sceneMap.calculate('middleTile');
+	var middleCor = this.game.sceneMap.twoDToScreen(middle.x,middle.y);
+	
+	this.text.setFontWeight('bold').setPosition(middleCor.x,middleCor.y).setText(
+			"This is your new farm, \n click on it to see \n your inventory");
+	
+	icon = new farming.Sprite('images/Farmer.png').setSize(100, 100).setPosition(middleCor.x,middleCor.y);
+	this.w.setFill('images/textbox/right_arrow.png').setSize(this.game.getFullSize(0.52).width, this.game.getFullSize(0.3).height)
+		.setPosition(middleCor.x, middleCor.y).setOpacity(0.85);	
+	this.introLayer.setOpacity(0.85);
+	this.introLayer.appendChild(this.w).appendChild(this.text).appendChild(icon);
+	
+	// Listen to the show_farm event
+	goog.events.listenOnce(this.game.source,this.game.EventType.SHOW_FARM,goog.partial(this.buttonAction,this.game.introduction));
+}
+
+
+farming.Introduction.prototype.write = function(text){
+	console.log('test text is: '+text);
 }
